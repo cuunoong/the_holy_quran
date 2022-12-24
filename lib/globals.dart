b1b5622f9103616f6c4a4dad99d3c7065c26683b
Arif Iskandar <arif19iskandar@gmail.com>
2022-12-25 06:39:17 +0700

    Adding gray color

--- lib/globals.dart
+++ lib/globals.dart
@@ -4,3 +4,4 @@ Color background = const Color(0xFF040C23);
 Color text = const Color(0xFFA19CC5);
 Color orange = const Color(0xFFF9B091);
 Color primary = const Color(0xFFA44AFF);
+Color gray = const Color(0xFF121931);
